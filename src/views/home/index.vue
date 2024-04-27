63965871112210ad44484eca7fe8cbfb1fe7ef88
黄勤坤 <hqk616@163.com>
2024-04-27 17:11:45 +0800

    project init

--- src/views/home/index.vue
+++ src/views/home/index.vue
@@ -1,11 +1,13 @@
 <template>
   <div>
-    <Home />
+
   </div>
 </template>
 
 <script setup lang="ts">
-import Home from '@/views/home/index.vue'
+
 </script>
 
-<style></style>
+<style scoped>
+
+</style>
