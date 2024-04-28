d3fdda7640f3d2c1d6d623d36d98a5ba93981458
黄勤坤 <hqk616@163.com>
2024-04-28 15:34:04 +0800

    opt: project

---
+++ src/components/Menu.vue
@@ -0,0 +1,104 @@
+ <template>
+  <section class="menu-wrapper">
+    <div>
+      <div class="menu-item bg-1">
+        <i class="iconfont icon-tip"></i>
+        <span>About</span>
+      </div>
+      <div class="menu-item bg-3">
+        <i class="iconfont icon-cpu"></i>
+        <span>Earn</span>
+      </div>
+    </div>
+
+    <div class="mine-btn">
+      <span class="mine-btn-text">Mine</span>
+    </div>
+
+    <div>
+      <div class="menu-item bg-2">
+        <i class="iconfont icon-frends"></i>
+        <span>Frends</span>
+      </div>
+      <div class="menu-item bg-4">
+        <i class="iconfont icon-rocket"></i>
+        <span>Boost</span>
+      </div>
+    </div>
+  </section>
+</template>
+
+<script setup lang="ts">
+
+</script>
+
+<style lang="scss" scoped>
+.menu-wrapper {
+  width: 100%;
+  display: flex;
+  padding: 0 1rem;
+  box-sizing: border-box;
+  justify-content: space-between;
+  & > * {
+    flex: 2;
+  }
+}
+.menu-item {
+  height: 60px;
+  color: #FFD49C;
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  align-items: center;
+  z-index: 5;
+  & + .menu-item {
+    margin-top: .625rem;
+  }
+  &.bg-1 {
+    background: url('@/assets/menu-item-1.png') no-repeat;
+  }
+  &.bg-2 {
+    background: url('@/assets/menu-item-2.png') no-repeat;
+  }
+  &.bg-3 {
+    background: url('@/assets/menu-item-3.png') no-repeat;
+  }
+  &.bg-4 {
+    background: url('@/assets/menu-item-4.png') no-repeat;
+  }
+}
+.mine-btn {
+  flex: 1;
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  position: relative;
+  &::before {
+    content: '';
+    position: absolute;
+    top: -0.625rem;
+    width: 166px;
+    height: 166px;
+    background: url('@/assets/button-platform.png') no-repeat center;
+    z-index: 1;
+  }
+  &::after {
+    content: '';
+    position: absolute;
+    top: .25rem;
+    left: -2rem;
+    width: 129px;
+    height: 129px;
+    background: url('@/assets/button-bg.png') no-repeat;
+    z-index: 1;
+  }
+  & .mine-btn-text {
+    z-index: 2;
+    font-size: 1.625rem;
+    background: linear-gradient(118.81126791266382deg, #5B320C 0%, #2C1206 100%);
+    -webkit-background-clip: text;
+    background-clip: text; 
+    color: transparent; 
+  }
+}
+</style>
--- src/views/home/index.vue
+++ src/views/home/index.vue
@@ -1,13 +1,60 @@
 <template>
-  <div>
+  <div class="app-container">
+    <!-- Ranking list -->
+    <section class="rank-wrapper">
+      <div class="rank-box curson-pointer">
+        <i class="iconfont icon-rank"></i>
+        <span class="rank-text">Rank</span>
+        <i class="iconfont icon-arrow_right"></i>
+      </div>
+    </section>
 
+    <!-- Logo -->
+    <section class="logo-wrapper">
+      <img src="@/assets/gold-mine.png" />
+      <span class="logo-text">EOB</span>
+    </section>
+
+    <!-- menu -->
+    <Menu />
   </div>
 </template>
 
 <script setup lang="ts">
+import Menu from '@/components/Menu.vue'
 
 </script>
 
-<style scoped>
+<style lang="scss" scoped>
+.rank-wrapper {
+  display: flex;
+  justify-content: end;
+  .rank-box {
+    background: #211F1D;
+    padding: .625rem .625rem .625rem 1rem;
+    border-radius: 1.75rem 0 0 1.75rem;
+    display: flex;
+    align-items: center; 
+    .rank-text {
+      color: rgba(255, 255, 255, 0.8);
+    }
+    & > i {
+      margin-right: 4px;
+    }
+  }
+}
 
+.logo-wrapper {
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  img {
+    width: 14px;
+    height: 14px;
+    margin-right: .25rem;
+  }
+  .logo-text {
+    font-size: 16px;
+  }
+}
 </style>
